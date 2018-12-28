d46a46252c29c1417aec27cdc3226f8b320b9461
emptydn1 <39210418+emptydn1@users.noreply.github.com>
2018-12-28 17:29:15 +0700

    Update Weathers.js

--- src/component/Weathers.js
+++ src/component/Weathers.js
@@ -6,15 +6,17 @@ class Weathers extends Component {
         var {name, weather_state_name, weather_state_abbr, applicable_date, date, min_temp, max_temp, the_temp } = this.props;
         if(this.props.name){
             return(
-                <div>
-                    <p>{name}</p>
-                    <p>{applicable_date}</p>
-                    <p>Max: {max_temp}</p>
-                    <p>Min: {min_temp}</p>
-                    <p>temp: {the_temp}</p>
-                    <div className="group d-flex justify-content-between">
-                        <p>{weather_state_name}</p>
-                        <div className="state-icon-sml state-s" style={{backgroundImage: `url(https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg)`}}>
+                <div className="card">
+                    <div className="card-body">
+                        <p>{name}</p>
+                        <p>{applicable_date}</p>
+                        <p>Max: {max_temp}</p>
+                        <p>Min: {min_temp}</p>
+                        <p>temp: {the_temp}</p>
+                        <div className="group d-flex justify-content-between">
+                            <p>{weather_state_name}</p>
+                            <div className="state-icon-sml state-s" style={{backgroundImage: `url(https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg)`}}>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -22,15 +24,17 @@ class Weathers extends Component {
         }
         else{
             return(
-                <div>
-                    <p>{date}</p>
-                    <p>{applicable_date}</p>
-                    <p>Max: {max_temp}</p>
-                    <p>Min: {min_temp}</p>
-                    <p>temp: {the_temp}</p>
-                    <div className="group d-flex justify-content-between">
-                        <p>{weather_state_name}</p>
-                        <div className="state-icon-sml state-s" style={{backgroundImage: `url(https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg)`}}>
+                <div className="card">
+                    <div className="card-body">
+                        <p>{date}</p>
+                        <p>{applicable_date}</p>
+                        <p>Max: {max_temp}</p>
+                        <p>Min: {min_temp}</p>
+                        <p>temp: {the_temp}</p>
+                        <div className="group d-flex justify-content-between">
+                            <p>{weather_state_name}</p>
+                            <div className="state-icon-sml state-s" style={{backgroundImage: `url(https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg)`}}>
+                            </div>
                         </div>
                     </div>
                 </div>
